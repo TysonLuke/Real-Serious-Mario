2ffc1ab581a16f55004ea97953b18a3cfd5c9794
Tyson Wetton <t.l.wetton@gmail.com>
2019-03-06 17:43:18 +1000

    Add loading of level from CSV file, with level controller.

---
+++ Assets/Scripts/Levels/LevelController.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RSM.WorldEntity;
+
+namespace RSM
+{
+    namespace Levels
+    {
+        public class LevelController : MonoBehaviour
+        {
+
+            public static LevelController Instance { get; private set; }
+
+            private List<Platform> disabled = new List<Platform>();
+
+            private float timeFading = 0f;
+
+            public float timeToFade = .5f;
+
+            public SpriteRenderer fadeImage;
+
+            /// <summary>
+            /// Set up level controller singleton.
+            /// </summary>
+            private void Awake()
+            {
+                if (Instance != null)
+                {
+                    DestroyImmediate(gameObject);
+                }
+                else
+                {
+                    Instance = this;
+                }
+            }
+
+            /// <summary>
+            /// Destroy the current level in preperation of loading a different level in.
+            /// </summary>
+            public void UnloadLevel()
+            {
+                for (int childIndex = transform.childCount - 1; childIndex >= 0; --childIndex)
+                {
+                    Destroy(transform.GetChild(childIndex).gameObject);
+                }
+            }
+
+            /// <summary>
+            /// Reload all objects to their original locations, ready for the player to play again.
+            /// </summary>
+            public void ReloadLevel()
+            {
+                EnableAll();
+            }
+
+            public void DisableObject(Platform platform)
+            {
+                // Add to this list of platforms that have been disabled.
+                disabled.Add(platform);
+            }
+
+            private void EnableAll()
+            {
+                foreach (Platform platform in disabled)
+                {
+                    platform.Enable();
+                }
+            }
+
+            public void NextLevel()
+            {
+                StartCoroutine("FadeIntoNewLevel");
+            }
+
+#region Different Fade Effects
+
+            public IEnumerator FadeOut()
+            {
+                Player.PlayerController.Instance.Movement.SetMovement(false);
+                while (timeFading < timeToFade)
+                {
+                    // If we're still fading, find the point of the fade we're at, and add deltaTime.
+                    fadeImage.color = new Color(0, 0, 0, timeFading / timeToFade);
+                    timeFading += Time.deltaTime;
+                    yield return null;
+                }
+                timeFading = 0f;
+                StartCoroutine("FadeIn");
+            }
+
+            public IEnumerator FadeOutOnly()
+            {
+                Player.PlayerController.Instance.Movement.SetMovement(false);
+                while (timeFading < timeToFade)
+                {
+                    // If we're still fading, find the point of the fade we're at, and add deltaTime.
+                    fadeImage.color = new Color(0, 0, 0, timeFading / timeToFade);
+                    timeFading += Time.deltaTime;
+                    yield return null;
+                }
+                timeFading = 0f;
+            }
+
+            public IEnumerator FadeIntoNewLevel()
+            {
+                Player.PlayerController.Instance.Movement.SetMovement(false);
+                while (timeFading < timeToFade)
+                {
+                    // If we're still fading, find the point of the fade we're at, and add deltaTime.
+                    fadeImage.color = new Color(0, 0, 0, timeFading / timeToFade);
+                    timeFading += Time.deltaTime;
+                    yield return null;
+                }
+                timeFading = 0f;
+                UnloadLevel();
+                GetComponent<LoadLevelFromFile>().LoadLevel();
+                StartCoroutine("FadeIn");
+            }
+
+            public IEnumerator FadeIn()
+            {
+                while (timeFading < timeToFade)
+                {
+                    // If we're still fading, find the point of the fade we're at, and add deltaTime.
+                    fadeImage.color = new Color(0, 0, 0, 1 - timeFading / timeToFade);
+                    timeFading += Time.deltaTime;
+                    yield return null;
+                }
+                timeFading = 0f;
+                Player.PlayerController.Instance.Movement.SetMovement(true);
+            }
+
+#endregion
+
+        }
+    }
+}
---
+++ Assets/Scripts/Levels/LoadLevelFromFile.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TysonLuke.Utilities;
+
+namespace RSM
+{
+    namespace Levels
+    {
+        public class LoadLevelFromFile : MonoBehaviour
+        {
+            public Dictionary<string, CSVData> levels = new Dictionary<string, CSVData>();
+
+            private string nextLevel = "0";
+
+            // The object we're using for level generation.
+            public GameObject ground, falling, enemy, headSmash, spike, checkpoint, endOfLevel, coin;
+
+            /// <summary>
+            /// Load the entire default folder on start, prepare the CSV files for use.
+            /// </summary>
+            public void Start()
+            {
+                // Get all levels from the default folder.
+                Object[] files;
+                files = Resources.LoadAll("", typeof(TextAsset));
+
+                // Load them all into a dictionary with their names so we can look them up.
+                foreach (var csv in files)
+                {
+                    CSVData current = new CSVData((TextAsset)csv);
+                    levels.Add(current.Cell(0, 1), current);
+                }
+
+                LoadLevel();
+            }
+
+            public void LoadLevel()
+            {
+                // If there is no file name given, assume we're loading default level named "0".
+                CSVData loading;
+                if (string.IsNullOrEmpty(nextLevel))
+                {
+                    loading = levels["0"];
+                }
+                else
+                {
+                    // Get the array from the CSV data.
+                    loading = levels[nextLevel];
+                }
+
+                // Take the name of the next level down.
+                nextLevel = loading.Cell(1, 1);
+
+                // Get the size of tiles this spreadsheet is using.
+                Vector2 tileSize = new Vector2(float.Parse(loading.Cell(2, 1)), float.Parse(loading.Cell(3, 1)));
+
+                Vector2 playerLocation = Vector2.zero;
+
+                float yOffset = (loading.RowCount() - 3) * tileSize.y;
+
+                for (int xAxis = 0; xAxis < loading.ColumnCount(); ++xAxis)
+                {
+                    for (int yAxis = 3; yAxis < loading.RowCount(); ++yAxis)
+                    {
+                        // The object we plan to place.
+                        GameObject toPlace = null;
+
+                        // If there is nothing in the cell, or it is just a newline they are useless to us.
+                        if (string.IsNullOrEmpty(loading.Cell(xAxis, yAxis)) || loading.Cell(xAxis, yAxis) == "\r")
+                        {
+                            continue;
+                        }
+
+                        float perTileY = 0;
+
+                        // Parse only the first character, as the final cell will contain newline characters.
+                        switch (loading.Cell(xAxis, yAxis)[0])
+                        {
+                            case 'g':
+                            {
+                                toPlace = ground;
+                                break;
+                            }
+                            case 'f':
+                            {
+                                toPlace = falling;
+                                break;
+                            }
+                            case 'e':
+                            {
+                                toPlace = enemy;
+                                break;
+                            }
+                            case 'h':
+                            {
+                                toPlace = headSmash;
+                                break;
+                            }
+                            case 's':
+                            {
+                                toPlace = spike;
+                                break;
+                            }
+                            case 'c':
+                            {
+                                toPlace = checkpoint;
+                                break;
+                            }
+                            case 'n':
+                            {
+                                // End of level tile is scaled differently to the rest of the objects, needs it's own offset.
+                                perTileY = tileSize.y / 2;
+                                toPlace = endOfLevel;
+                                break;
+                            }
+                            case 'p':
+                            {
+                                playerLocation = new Vector2(xAxis * tileSize.x, (-yAxis * tileSize.y) + yOffset);
+                                continue;
+                            }
+                            case 'o':
+                            {
+                                toPlace = coin;
+                                break;
+                            }
+                            default:
+                            {
+                                // If we ended up here, it means we haven't handled something correctly, and should find out what it was.
+                                Debug.LogError("Cell contents were not handled at " + xAxis + ", " + yAxis + " contents are: " + loading.Cell(xAxis, yAxis));
+                                break;
+                            }
+                        }
+
+                        // Tell the player to get in position and update it's info.
+                        RSM.Player.PlayerController.Instance.LevelLoaded(playerLocation);
+
+                        Instantiate(toPlace, new Vector2(xAxis * tileSize.x, (-yAxis * tileSize.y) + yOffset + perTileY), Quaternion.identity, gameObject.transform);
+                    }
+                }
+
+            }
+        }
+    }
+}
